d80fd47bf503afcd67193a430fdc0c590bbb765c
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-11-05 20:29:54 -0600

    Complete the exercise for circles.

--- circles/reducer.js
+++ circles/reducer.js
@@ -17,11 +17,31 @@ const initialState = [
     },
 ];
 
+
+
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
+
     const { type } = action;
-
+    console.log('Here is the action:', action)
     // ACTION: Add a random circle
-
+    if (type === "ADDCIRCLE") {
+        return [
+            ...state,
+            {
+                radius: Math.random()*100,
+                color: getRandomColor()
+            }
+        ]
+    }
     return state;
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
