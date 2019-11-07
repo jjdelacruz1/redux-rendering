ce2c9207521053e9d60393b44c468ae408f24dee
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-11-06 21:11:09 -0600

    Complete poker exercise.

--- poker/reducer.js
+++ poker/reducer.js
@@ -14,6 +14,35 @@ const reducer = (state = initialState, action) => {
     const { type } = action;
 
     // ACTION - Change either card to a random new one (Try implementing one action that both buttons dispatch)
+    if (type === "RANDOMCARD1") {
+        return [
+            {
+                value: randomValue(),
+                suit: randomSuit()
+            },
+            state[1]
+        ]
+    } else if (type === "RANDOMCARD2") {
+        return [
+            state[0],
+            {
+                value: randomValue(),
+                suit: randomSuit()
+            }
+        ]
+    }
     
     return state;
+}
+
+function randomValue ( ) {
+    const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    let randomValue = cardValue[Math.floor(Math.random()*cardValue.length)]
+    return randomValue
+}
+
+function randomSuit ( ) {
+    const cardSuit = ['C', 'D', 'H', 'S']
+    let randomSuit = cardSuit[Math.floor(Math.random()*cardSuit.length)]
+    return randomSuit
 }
