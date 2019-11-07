d67797eb173188e5f4bfd47ee07645834cf50be2
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-11-06 19:53:53 -0600

    Fix nametag to clear input field after pressing enter.

--- nameTags/index.js
+++ nameTags/index.js
@@ -6,12 +6,13 @@ subscribe(() => render(getState()));
 dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
-
+const nameTxt = document.getElementById('input')
 const newName = document.getElementById('nameForm');
 newName.addEventListener('submit', function(e) {
   // console.log(newName[0].value, "you pressed enter!" )
   e.preventDefault()
   dispatch({ type:"ADDNAME" })
+  nameTxt.value = ''
   // console.dir(newName[0].value)
 })
 
--- poker/index.js
+++ poker/index.js
@@ -7,3 +7,10 @@ dispatch({ type: null }); // Here we're making a call to dispatch() - this trigg
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
 
+const card1 = document.getElementById('card1Button');
+card1.addEventListener('click', e => dispatch({ type: "RANDOMCARD1" }))
+
+const card2 = document.getElementById('card2Button');
+card2.addEventListener('click', e => dispatch({ type: "RANDOMCARD2" }))
+
+
